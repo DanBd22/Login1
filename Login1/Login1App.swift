4b37e5bef9a20568712443eb8da4f9db3fbd4441
DanBd22 <dan.badarau04@gmail.com>
2023-04-23 14:28:48 +0300

    Initial Commit

---
+++ Login1/Login1App.swift
@@ -0,0 +1,17 @@
+//
+//  Login1App.swift
+//  Login1
+//
+//  Created by Badarau Dan on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Login1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
